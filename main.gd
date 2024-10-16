daad7a2518143818dd9a80589c879aa8955e84f9
Lugi-alo <ejolohan@gmail.com>
2024-10-16 15:38:49 +0100

    updated main functionality to generate items from users clicks

--- main.gd
+++ main.gd
@@ -1,23 +1,40 @@
 extends Node2D
 
-# Cake Declarations
-@onready var strawberrySlice = $strawberry_area 
-@onready var redVelvetSlice = $red_velvet_area    
+@onready var ovenArea = $oven_area
+@onready var strawberrySlice = preload("res://assets/strawberry slice.png")  
 
 func _ready():
-	# Connecting input event signals to handle clicks
-	strawberrySlice.connect("input_event", Callable(self, "_on_strawberry_area_input_event"))
-	redVelvetSlice.connect("input_event", Callable(self, "_on_red_velvet_area_input_event"))
+	ovenArea.connect("input_event", Callable(self, "_on_oven_area_input_event"))
 
-
-# Test functions
-# Functions will be later needed for moving the items onto a counter, or seeing amount left
-# Function will need to be modified so cakes are generated from cooking event
-
-func _on_strawberry_area_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
+# Function altered to generate cake items.
+# Needs to be changed to allow users to pick from a selection
+func _on_oven_area_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
 	if _event is InputEventMouseButton and _event.pressed:
-		print("Strawberry Cake Clicked!")
+		# Debug statement
+		print("Oven Clicked! Starting to make a strawberry cake...")
+		start_cooking("Strawberry Cake")
 
-func _on_red_velvet_area_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
-	if _event is InputEventMouseButton and _event.pressed:
-		print("Red Velvet Cake Clicked!")
+# Needs to eventually display timer above the oven
+func start_cooking(cake_name: String) -> void:
+	# Debug statement
+	print("Starting countdown as 5 seconds...")
+	var cooking_time = 5.0
+	# Debug statement
+	print("Cooking " + cake_name + " for " + str(cooking_time) + " seconds...")
+	await get_tree().create_timer(cooking_time).timeout
+	# Waits for countdown to end
+	place_cake_on_oven(cake_name)
+	
+func place_cake_on_oven(cake_name: String) -> void:
+	# Debug statement
+	print("Strawberry cake is ready!")
+	
+	var cake_sprite = Sprite2D.new()
+	cake_sprite.texture = strawberrySlice
+	cake_sprite.position = ovenArea.position
+	cake_sprite.z_index = 10
+	add_child(cake_sprite)
+	
+	# Debug statement
+	print(cake_name + " is ready and placed on the oven")
+	
